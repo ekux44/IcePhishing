fbe77eed482c0a55695e7fabd92c05af16f05118
Eric Kuxhausen <erickuxhausen+git@gmail.com>
2013-11-15 20:44:26 -0600

    Initial Commit

---
+++ IcePhishing/icemenAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  icemenAppDelegate.h
+//  IcePhishing
+//
+//  Created by Eric Kuxhausen on 11/15/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface icemenAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IcePhishing/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IcePhishing
+//
+//  Created by Eric Kuxhausen on 11/15/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "icemenAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([icemenAppDelegate class]));
+    }
+}
